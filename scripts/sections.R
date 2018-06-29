926541195af0fc05b8649c1a4976121506d6b3ad
Duncan Temple Lang <duncan@r-project.org>
2018-06-29 13:28:33 -0700

    get sections script

---
+++ scripts/sections.R
@@ -0,0 +1,9 @@
+
+tiff = list.files(pattern = "\\.tiff$", full = TRUE, recursive = TRUE)
+base = gsub("_[0-9]+\\.tiff", "", tiff)
+txtFiles = split(gsub("\\.tiff$", ".txt", tiff), base)
+
+source("ZooScanned/R/fromText.R")
+tmp = lapply(txtFiles[sects], getDocElements)
+
+#z = tmp[names(z)]
