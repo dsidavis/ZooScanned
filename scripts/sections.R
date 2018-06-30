e9e7a43d038f8123898d6c6c91e553e3104bc0c8
Duncan Temple Lang <duncan@r-project.org>
2018-06-29 17:06:15 -0700

    extra piece

--- scripts/sections.R
+++ scripts/sections.R
@@ -2,6 +2,10 @@
 tiff = list.files(pattern = "\\.tiff$", full = TRUE, recursive = TRUE)
 base = gsub("_[0-9]+\\.tiff", "", tiff)
 txtFiles = split(gsub("\\.tiff$", ".txt", tiff), base)
+txt = tapply(tiff, base, function(x) unlist(lapply(gsub("\\.tiff$", ".txt", x), readLines, warn = FALSE)))
+
+sects = sapply(txt, function(x) any(grepl("^((([0-9.]+ )? [A-Z ]+)|Discussion|Introduction|Materials and Methods|References)$", x, ignore.case = TRUE)))
+
 
 source("ZooScanned/R/fromText.R")
 tmp = lapply(txtFiles[sects], getDocElements)
