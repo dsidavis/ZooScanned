25bc624834039b6edfeb17ce4bfd872b6436b625
Duncan Temple Lang <duncan@r-project.org>
2018-07-07 07:04:37 -0700

    we need to identify just the .txt files we generated directly from tesseract, not the ones in ZooScanned/testData/ that were built by hand. These are different.

--- scripts/pickdocs.R
+++ scripts/pickdocs.R
@@ -1,6 +1,6 @@
 # Since I have all txt files downloaded, and I currently don't have enough space to unzip all tiff files
 # I think I can find all txt files directly by this line, is that right?
-txt = list.files(pattern = "\\.txt$", full = TRUE, recursive = TRUE)
+txt = list.files(pattern = "_[0-9]+\\.txt$", full = TRUE, recursive = TRUE)
 
 base = gsub("_[0-9]+\\.txt", "", txt)
 txtFiles = split(txt, base)
