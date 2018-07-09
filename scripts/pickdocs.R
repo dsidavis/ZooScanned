c94ed719387b8ee58641d5b6eb460ced171ad4e0
WillJu <juwangqian2015@gmail.com>
2018-07-08 21:32:44 -0700

    select docs by the number of sections found by getDocElements

--- scripts/pickdocs.R
+++ scripts/pickdocs.R
@@ -15,3 +15,38 @@ tmp = lapply(txtFiles, getDocElements)
 # getDocElements drops lots of stuffs and has Errir in !ok
 
 tmp
+
+all.length = sapply(tmp, length)
+table(all.length)
+plot(density(all.length))
+
+ind.min = which(all.length %in% c(1, 2, 3))
+ind.mid = which(all.length %in% c(9))
+ind.max = which(all.length %in% c(18, 19, 22))
+
+lapply(tmp[ind.min], names)
+lapply(tmp[ind.mid], names)
+lapply(tmp[ind.max], names)
+
+# Min:
+# scannedNewPDFs_txt/Isla Vista Virus/Song-1995 
+# scannedNewPDFs_txt/Lagos Bat Virus/Shope-1982
+# scannedNewPDFs_txt/Issyk-Kul/Lvov-1973
+
+# Mid:
+# scannedNewPDFs_txt/Black Creek Canal Virus/Rollin-1995
+# scannedNewPDFs_txt/European Bat Lyssavirus 2/Whitby-2000
+
+# max:
+# scannedNewPDFs_txt/Crimean Congo Hemorrhagic Fever Virus/Swanepoel-1987
+# scannedNewPDFs_txt/Western equine encephalitis/Iversson-1993
+# scannedNewPDFs_txt/Jamestown Canyon Virus/Clark-1986
+# scannedNewPDFs_txt/Jamestown Canyon Virus/Reisen-1995
+
+
+
+
+
+
+
+
